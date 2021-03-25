5b29f6c681b2e1493358a9dca17c6fc8c1e14ac8
Thalía <thaliagpineda99@gmail.com>
2021-03-25 20:46:30 +0100

    Modificación ej-07

--- P2/S5/Ej-07.js
+++ P2/S5/Ej-07.js
@@ -1,35 +1,3 @@
-const gui = {
-    display : document.getElementById("display"),
-    start : document.getElementById("start"),
-    stop : document.getElementById("stop"),
-    reset : document.getElementById("reset")
-}
-
-console.log("Ejecuitando JS...");
-
-//-- Definir un objeto cronómetro
-const crono = new Crono(gui.display);
-
-//---- Configurar las funciones de retrollamada
-
-//-- Arranque del cronometro
-gui.start.onclick = () => {
-    console.log("Start!!");
-    crono.start();
-}
-  
-//-- Detener el cronómetro
-gui.stop.onclick = () => {
-    console.log("Stop!");
-    crono.stop();
-}
-
-//-- Reset del cronómetro
-gui.reset.onclick = () => {
-    console.log("Reset!");
-    crono.reset();
-}
-
 //-- Clase cronómetro
 class Crono {
 
@@ -94,3 +62,36 @@ class Crono {
         this.display.innerHTML = "0:0:0";
     }
 }
+
+const gui = {
+    display : document.getElementById("display"),
+    start : document.getElementById("start"),
+    stop : document.getElementById("stop"),
+    reset : document.getElementById("reset")
+}
+
+console.log("Ejecuitando JS...");
+
+//-- Definir un objeto cronómetro
+const crono = new Crono(gui.display);
+
+//---- Configurar las funciones de retrollamada
+
+//-- Arranque del cronometro
+gui.start.onclick = () => {
+    console.log("Start!!");
+    crono.start();
+}
+  
+//-- Detener el cronómetro
+gui.stop.onclick = () => {
+    console.log("Stop!");
+    crono.stop();
+}
+
+//-- Reset del cronómetro
+gui.reset.onclick = () => {
+    console.log("Reset!");
+    crono.reset();
+}
+
