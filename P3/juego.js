2b0b4269d93be20c0420c7fd0b247cbc5386a310
Thalía <thaliagpineda99@gmail.com>
2021-05-28 18:34:42 +0200

    Añadiendo bola

--- P3/juego.js
+++ P3/juego.js
@@ -50,4 +50,24 @@ for (let i = 0; i < LADRILLO.F; i++) {
     }
 }
 
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
+function DibujarBola() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "orange";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function MoverBola() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
