7abb829850f8e6b7a7bcfde8b726f8da97571642
Thalía <thaliagpineda99@gmail.com>
2021-05-30 17:27:27 +0200

    Pelota en movimiento, raqueta sin movimiento

--- P3/juego.js
+++ P3/juego.js
@@ -8,6 +8,16 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
+//-- Raqueta
+var AnchuraRaqueta = 70;
+var AlturaRaqueta = 9;
+var Raqueta_EjeX = (canvas.width-AnchuraRaqueta)/2;
+
+//-- Controles raqueta
+var FlechaDerecha = false;
+var FlechaIzquierda = false;
+
+
 //-- Creamos una funcion para dibujar la bola y hacer que se mueva
 function DibujarBola() {
   ctx.beginPath();
@@ -17,10 +27,20 @@ function DibujarBola() {
   ctx.closePath;
 }
 
+//-- Creamos una funcion para dibujar la raqueta
+function DibujarRaqueta(){
+  ctx.beginPath();
+  ctx.rect(Raqueta_EjeX, canvas.height-AlturaRaqueta, AnchuraRaqueta, AlturaRaqueta);
+  ctx.fillStyle = "orange";
+  ctx.fill();
+  ctx.closePath();
+}
+
 //-- Creamos una funcion para que solo se vea la bola y no toda la trayecoria
 function EliminarTrayectoria() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   DibujarBola();
+  DibujarRaqueta();
 
   if(x + dx > canvas.width-RadioBola || x + dx < RadioBola){
     dx = -dx;
@@ -28,9 +48,39 @@ function EliminarTrayectoria() {
   if(y + dy > canvas.height-RadioBola || y + dy < RadioBola){
     dy = -dy;
   }
-  
+  if(FlechaDerecha && Raqueta_EjeX < canvas.width-AnchuraRaqueta){
+    Raqueta_EjeX += 7;
+  }
+  else if(FlechaIzquierda && Raqueta_EjeX > 0){
+    Raqueta_EjeX -= 7;
+  }
+
   x += dx;
   y += dy;
+}
+
+//-- Saber que flecha se esta pulsando
+document.addEventListener("teclapulsada", TeclaPulsada, false);
+document.addEventListener("teclalevantada", TeclaLevantada, false);
+
+//-- Creamos la funcion tecla pulsada
+function TeclaPulsada(e){
+  if(e.tecla == 39){
+    FlechaDerecha = true;
+  }
+  else if(e.tecla == 37){
+    FlechaIzquierda = true;
+  }
+}
+
+//-- Creamos la funcion tecla levantada
+function TeclaLevantada(e){
+  if(e.tecla == 39){
+    FlechaDerecha = false;
+  }
+  else if(e.tecla == 37){
+    FlechaIzquierda = false;
+  }
 }
 
 setInterval(EliminarTrayectoria, 10);
